c0b5ad1d785f54f8c29e148d1fc051e02a719dc5
wumin <wumin@114study.com>
2019-07-25 17:34:30 +0800

    20190725

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,8 @@
 const path = require('path')
-// const appDate = require('./data.json')
-// const seller = appDate.seller
-// const ratings = appDate.ratings
-// const goods = appDate.goods
+const appDate = require('./data.json')
+const seller = appDate.seller
+const ratings = appDate.ratings
+const goods = appDate.goods
 
 function resolve (dir) {
   return path.join(__dirname, dir)
@@ -45,37 +45,37 @@ module.exports = {
   },
   devServer: {
     // 设置代理，解决跨域问题，只在开发环境下有效，生产环境下要将请求的baseurl配置为服务器端路径
-    proxy: {
-      '/api': {
-        target: 'http://192.168.1.136:8081',
-        changeOrigin: true,
-        // 对/api进行重写
-        pathRewrite: {
-          '^/api': '/api'
-        }
-      }
-    }
-    // 简单mock数据
-    // before (app) {
-    //   app.get('/api/seller', function (req, res) {
-    //     res.json({
-    //       errno: 0,
-    //       data: seller
-    //     })
-    //   })
-    //   app.get('/api/ratings', function (req, res) {
-    //     res.json({
-    //       errno: 0,
-    //       data: ratings
-    //     })
-    //   })
-    //   app.get('/api/goods', function (req, res) {
-    //     res.json({
-    //       errno: 0,
-    //       data: goods
-    //     })
-    //   })
+    // proxy: {
+    //   '/api': {
+    //     target: 'http://192.168.1.136:8080',
+    //     changeOrigin: true,
+    //     // 对/api进行重写
+    //     pathRewrite: {
+    //       '^/api': '/api'
+    //     }
+    //   }
     // }
+    // 简单mock数据
+    before (app) {
+      app.get('/api/seller', function (req, res) {
+        res.json({
+          errno: 0,
+          data: seller
+        })
+      })
+      app.get('/api/ratings', function (req, res) {
+        res.json({
+          errno: 0,
+          data: ratings
+        })
+      })
+      app.get('/api/goods', function (req, res) {
+        res.json({
+          errno: 0,
+          data: goods
+        })
+      })
+    }
   },
 
   pluginOptions: {
